e5425d70be4de5910c6c31d0398fb874e629eacf
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 02:18:54 +0700

    New: comment forbidden access error test

---
+++ src/application/comments/errors/comment-forbidden-access.error.spec.ts
@@ -0,0 +1,17 @@
+import { DomainError } from '@main/domain/common/domain-error';
+import { CommentForbiddenAccessError } from './comment-forbidden-access.error';
+
+describe('CommentForbiddenAccessError', () => {
+  it('should be instance of DomainError', () => {
+    const error = new CommentForbiddenAccessError();
+    expect(error).toBeInstanceOf(DomainError);
+  });
+
+  it('should correctly initialize properties', () => {
+    const error = new CommentForbiddenAccessError('forbidden');
+
+    expect(error.name).toBe('CommentForbiddenAccessError');
+    expect(error.message).toBe('forbidden');
+    expect(error.code).toBe('FORBIDDEN_ACCESS');
+  });
+});
