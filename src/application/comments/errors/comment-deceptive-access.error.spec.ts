63bd8fbb81654e9cd8ab5184263cf9abd3c5e133
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 02:12:53 +0700

    New: comment deceptive access error test

---
+++ src/application/comments/errors/comment-deceptive-access.error.spec.ts
@@ -0,0 +1,17 @@
+import { DomainError } from '@main/domain/common/domain-error';
+import { CommentDeceptiveAccessError } from './comment-deceptive-access.error';
+
+describe('CommentDeceptiveAccessError', () => {
+  it('should be instance of DomainError', () => {
+    const error = new CommentDeceptiveAccessError();
+    expect(error).toBeInstanceOf(DomainError);
+  });
+
+  it('should correctly initialize properties', () => {
+    const error = new CommentDeceptiveAccessError();
+
+    expect(error.name).toBe('CommentDeceptiveAccessError');
+    expect(error.message).toBe('Cannot access comment');
+    expect(error.code).toBe('DECEPTIVE_ACCESS');
+  });
+});
