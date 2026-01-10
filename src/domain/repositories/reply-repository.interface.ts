e0f82b1efa060f5758ad1f8387ee486cad70dbed
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 00:57:55 +0700

    Remove update by id method

--- src/domain/repositories/reply-repository.interface.ts
+++ src/domain/repositories/reply-repository.interface.ts
@@ -3,6 +3,5 @@ import { Reply, ReplyId } from '../entities/reply';
 export interface ReplyRepository {
   add(reply: Reply): Promise<void>;
   findById(id: ReplyId): Promise<Reply | null>;
-  updateById(id: ReplyId, changes: Partial<Reply>): Promise<void>;
   softDelete(reply: Reply): Promise<void>;
 }
