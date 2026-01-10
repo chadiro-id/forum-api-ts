4398daa789d9850071a46854cd1900e286e9a3ec
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 03:26:17 +0700

    Add more test cases

--- src/domain/entities/thread.spec.ts
+++ src/domain/entities/thread.spec.ts
@@ -25,15 +25,32 @@ describe('Thread Entity', () => {
   const body = 'Isi thread';
   const creationDate = new Date();
 
-  describe('constructor', () => {
+  describe('create', () => {
     it('should create instance with valid data', () => {
-      const thread = new Thread(id, ownerId, title, body, creationDate);
+      const thread = Thread.create(id, ownerId, title, body, creationDate);
 
       expect(thread.id).toStrictEqual(id);
       expect(thread.ownerId).toStrictEqual(ownerId);
       expect(thread.title).toBe(title);
       expect(thread.body).toBe(body);
       expect(thread.createdAt).toStrictEqual(creationDate);
+    });
+
+    it('should provide default value creation date', () => {
+      const thread = Thread.create(id, ownerId, title, body);
+
+      const timeDiff = Math.abs(Date.now() - thread.createdAt.getTime());
+      expect(timeDiff < 10000).toBe(true);
+    });
+
+    it('should throw DomainError when title more than 255 character', () => {
+      try {
+        Thread.create(id, ownerId, 'a'.repeat(256), body);
+      } catch (error) {
+        expect(error).toBeInstanceOf(DomainError);
+        expect(error.message).toBe('Title max 255 character');
+        expect(error.code).toBe('THREAD_INVALID_TITLE');
+      }
     });
   });
 });
