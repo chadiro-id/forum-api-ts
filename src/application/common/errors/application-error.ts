84089d879699f33b88302c46b20b89eaf090a790
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 13:03:23 +0700

    Update error code

--- src/application/common/errors/application-error.ts
+++ src/application/common/errors/application-error.ts
@@ -1,20 +1,17 @@
 import { DomainError } from '../../../domain/common/domain-error';
 
 export type ApplicationErrorCode =
-  | 'APPLICATION_ERROR'
   | 'INPUT_VALIDATION_ERROR'
-  | 'USERNAME_ALREADY_EXISTS'
-  | 'NON_EXISTENCE_USER_LOGIN'
-  | 'INVALID_LOGIN_CREDENTIALS'
-  | 'NOT_FOUND_ERROR'
+  | 'USERNAME_ALREADY_EXISTS_ERROR'
+  | 'NON_EXISTENCE_USER_LOGIN_ERROR'
+  | 'INVALID_LOGIN_CREDENTIALS_ERROR'
+  | 'KEY_NOT_FOUND_ERROR'
   | 'DECEPTIVE_ACCESS_ERROR'
-  | 'NOT_AUTHORIZED_ERROR';
+  | 'UNAUTHORIZED_ACCESS_ERROR'
+  | 'INVALID_OPERATIONS_ERROR';
 
 export class ApplicationError extends DomainError {
-  constructor(
-    message: string,
-    code: ApplicationErrorCode = 'APPLICATION_ERROR',
-  ) {
+  constructor(message: string, code: ApplicationErrorCode) {
     super(message, code);
     this.name = 'ApplicationError';
   }
