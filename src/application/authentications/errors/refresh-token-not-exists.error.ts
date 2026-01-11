b8da43fdf3c85dbe45cbe66b254e2fbed5a1b33c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 18:49:36 +0700

    Rename class name

--- src/application/authentications/errors/refresh-token-not-exists.error.ts
+++ src/application/authentications/errors/refresh-token-not-exists.error.ts
@@ -1,8 +1,8 @@
-import { ApplicationError } from '@main/application/common/errors/application-error';
+import { ApplicationError } from '../../common/errors/application-error';
 
-export class RefreshTokenNotFoundError extends ApplicationError {
+export class RefreshTokenNotExistsError extends ApplicationError {
   constructor(message?: string) {
     super(message || 'token not exists', 'NON_EXISTENCE_ERROR');
-    this.name = 'RefreshTokenNotFoundError';
+    this.name = 'RefreshTokenNotExistsError';
   }
 }
