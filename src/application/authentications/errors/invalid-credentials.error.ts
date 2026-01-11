9d96ffab8dfb45125a381002faff683221307d97
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 17:10:24 +0700

    update inheritance

--- src/application/authentications/errors/invalid-credentials.error.ts
+++ src/application/authentications/errors/invalid-credentials.error.ts
@@ -1,8 +1,8 @@
-import { DomainError } from '../../../domain/common/domain-error';
+import { ApplicationError } from '../../common/errors/application-error';
 
-export class InvalidCredentialsError extends DomainError {
-  constructor(message?: string) {
-    super(message, 'INVALID_CREDENTIALS');
+export class InvalidCredentialsError extends ApplicationError {
+  constructor(message: string = '') {
+    super(message, 'INVALID_LOGIN_CREDENTIALS_ERROR');
     this.name = 'InvalidCredentialsError';
   }
 }
