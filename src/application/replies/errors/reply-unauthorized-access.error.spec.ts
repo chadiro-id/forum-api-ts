4bb7b0012148d78f005768329b8088b13152e4dc
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 15:07:30 +0700

    update test cases

--- src/application/replies/errors/reply-unauthorized-access.error.spec.ts
+++ src/application/replies/errors/reply-unauthorized-access.error.spec.ts
@@ -1,14 +1,14 @@
 import { DomainError } from '../../../domain/common/domain-error';
-import { ReplyForbiddenAccessError } from './reply-forbidden-access.error';
+import { ReplyUnauthorizedAccessError } from './reply-unauthorized-access.error';
 
-describe('ReplyForbiddenAccessError', () => {
-  it('should be instance of DomainError', () => {
-    const error = new ReplyForbiddenAccessError();
+describe('ReplyUnauthorizedAccessError', () => {
+  it('should be instance of ApplicationError', () => {
+    const error = new ReplyUnauthorizedAccessError('');
     expect(error).toBeInstanceOf(DomainError);
   });
 
   it('should correctly initialize properties', () => {
-    const error = new ReplyForbiddenAccessError('forbidden');
+    const error = new ReplyUnauthorizedAccessError('');
 
     expect(error.name).toBe('ReplyForbiddenAccessError');
     expect(error.message).toBe('forbidden');
