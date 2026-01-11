48445cb7e13be5f6ba72a602eaed2429a6d42943
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 17:15:24 +0700

    update inheritance

--- src/application/authentications/errors/non-existence-user-login.error.ts
+++ src/application/authentications/errors/non-existence-user-login.error.ts
@@ -1,8 +1,8 @@
-import { DomainError } from '../../../domain/common/domain-error';
+import { ApplicationError } from '@main/application/common/errors/application-error';
 
-export class NonExistenceUserLoginError extends DomainError {
-  constructor(message?: string) {
-    super(message, 'NON_EXISTENCE_USER_LOGIN');
+export class NonExistenceUserLoginError extends ApplicationError {
+  constructor(message: string = '') {
+    super(message, 'NON_EXISTENCE_USER_LOGIN_ERROR');
     this.name = 'NonExistenceUserLoginError';
   }
 }
