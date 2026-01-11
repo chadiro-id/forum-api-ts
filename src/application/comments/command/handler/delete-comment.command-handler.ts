0fe390b74833757698c7f5ebaa9a947982f91fce
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 14:01:30 +0700

    update delete verifications error

--- src/application/comments/command/handler/delete-comment.command-handler.ts
+++ src/application/comments/command/handler/delete-comment.command-handler.ts
@@ -1,5 +1,5 @@
+import { ApplicationError } from '@main/application/common/errors/application-error';
 import { CommentRepository } from '../../../../domain/repositories/comment-repository.interface';
-import { CommentNotFoundError } from '../../errors/comment-not-found.error';
 import { DeleteCommentCommand } from '../delete-comment.command';
 
 export class DeleteCommentCommandHandler {
@@ -7,13 +7,26 @@ export class DeleteCommentCommandHandler {
 
   async handle(command: DeleteCommentCommand): Promise<void> {
     const comment = await this.commentRepository.findById(command.id);
-    if (comment === null) {
-      throw new CommentNotFoundError();
+    if (comment === null || comment.isDelete) {
+      throw new ApplicationError(
+        'komentar tidak ditemukan',
+        'KEY_NOT_FOUND_ERROR',
+      );
     }
 
-    comment.verifyNonDeceptiveAccess(command.threadId);
-    comment.verifyAccessRights(command.userId);
-    comment.markAsDeleted();
+    if (!comment.threadId.equals(command.threadId)) {
+      throw new ApplicationError(
+        'tidak dapat mengakses sumber daya',
+        'DECEPTIVE_ACCESS_ERROR',
+      );
+    }
+
+    if (!comment.ownerId.equals(command.userId)) {
+      throw new ApplicationError(
+        'pengguna tidak memiliki hak akses',
+        'UNAUTHORIZED_ACCESS_ERROR',
+      );
+    }
 
     await this.commentRepository.softDelete(comment);
   }
