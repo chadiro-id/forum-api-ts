d02c8042467d77226eb02f7b95422a54361be64f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 05:40:58 +0700

    new: userlogout command handler test

---
+++ src/application/authentications/command/handler/user-logout.command-handler.spec.ts
@@ -0,0 +1,52 @@
+import { AuthenticationRepository } from '@main/domain/repositories/authentication-repository.interface';
+import { MockAuthenticationRepository } from '@main/application/common/tests/mock-authentication-repository';
+import { UserLogoutCommandHandler } from './user-logout.command-handler';
+import {
+  Authentication,
+  AuthenticationId,
+} from '@main/domain/entities/authentication';
+import { UserId } from '@main/domain/entities/user';
+import { UserLogoutCommand } from '../user-logout.command';
+import { RefreshTokenNotExistsError } from '../../errors/refresh-token-not-exists.error';
+
+describe('UserLogoutCommandHandler', () => {
+  let mockAuthRepo: AuthenticationRepository;
+  let commandHandler: UserLogoutCommandHandler;
+
+  beforeAll(() => {
+    mockAuthRepo = new MockAuthenticationRepository();
+    commandHandler = new UserLogoutCommandHandler(mockAuthRepo);
+  });
+
+  it('should handle user logout correctly', async () => {
+    const id = new AuthenticationId(1);
+    const userId = new UserId('user-001');
+    const mockAuthEntity = Authentication.create(userId, 'refresh_token');
+    mockAuthEntity.assignId(id);
+
+    const calledAuthEntity = Authentication.create(userId, 'refresh_token');
+    calledAuthEntity.assignId(id);
+
+    mockAuthRepo.findByToken = jest.fn().mockResolvedValue(mockAuthEntity);
+    mockAuthRepo.delete = jest.fn().mockResolvedValue(undefined);
+
+    const command = new UserLogoutCommand('refresh_token');
+    await commandHandler.handle(command);
+
+    expect(mockAuthRepo.findByToken).toHaveBeenCalledWith('refresh_token');
+    expect(mockAuthRepo.delete).toHaveBeenCalledWith(calledAuthEntity);
+  });
+
+  it('should throw error when refresh token not exists', async () => {
+    mockAuthRepo.findByToken = jest.fn().mockResolvedValue(null);
+    mockAuthRepo.delete = jest.fn();
+
+    const command = new UserLogoutCommand('refresh_token');
+    await expect(commandHandler.handle(command)).rejects.toThrow(
+      RefreshTokenNotExistsError,
+    );
+
+    expect(mockAuthRepo.findByToken).toHaveBeenCalledWith('refresh_token');
+    expect(mockAuthRepo.delete).not.toHaveBeenCalled();
+  });
+});
