cd659025bcdd7565e5b805a9d9c1386fc6c7a525
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 14:38:18 +0700

    update test cases

--- src/application/comments/errors/comment-forbidden-access.error.spec.ts
+++ src/application/comments/errors/comment-forbidden-access.error.spec.ts
@@ -1,17 +1,17 @@
-import { DomainError } from '../../../domain/common/domain-error';
-import { CommentForbiddenAccessError } from './comment-unauthorized-access.error';
+import { ApplicationError } from '../../common/errors/application-error';
+import { CommentUnauthorizedAccessError } from './comment-unauthorized-access.error';
 
 describe('CommentForbiddenAccessError', () => {
-  it('should be instance of DomainError', () => {
-    const error = new CommentForbiddenAccessError();
-    expect(error).toBeInstanceOf(DomainError);
+  it('should be instance of ApplicationError', () => {
+    const error = new CommentUnauthorizedAccessError('');
+    expect(error).toBeInstanceOf(ApplicationError);
   });
 
   it('should correctly initialize properties', () => {
-    const error = new CommentForbiddenAccessError('forbidden');
+    const error = new CommentUnauthorizedAccessError('unauthorized');
 
-    expect(error.name).toBe('CommentForbiddenAccessError');
-    expect(error.message).toBe('forbidden');
-    expect(error.code).toBe('FORBIDDEN_ACCESS');
+    expect(error.name).toBe('CommentUnauthorizedAccessError');
+    expect(error.message).toBe('unauthorized');
+    expect(error.code).toBe('UNAUTHORIZED_ACCESS_ERROR');
   });
 });
