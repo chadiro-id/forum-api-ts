a46ceb10cf334f3ae4224e902141be8d7f59415c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 18:47:09 +0700

    update default message

--- src/application/authentications/errors/invalid-credentials.error.ts
+++ src/application/authentications/errors/invalid-credentials.error.ts
@@ -1,8 +1,8 @@
 import { ApplicationError } from '../../common/errors/application-error';
 
 export class InvalidCredentialsError extends ApplicationError {
-  constructor(message: string = '') {
-    super(message, 'AUTHENTICATION_ERROR');
+  constructor(message?: string) {
+    super(message || 'Invalid credentials', 'AUTHENTICATION_ERROR');
     this.name = 'InvalidCredentialsError';
   }
 }
--- src/application/authentications/errors/refresh-token-not-found.error.ts
+++ src/application/authentications/errors/refresh-token-not-found.error.ts
@@ -1,8 +1,8 @@
 import { ApplicationError } from '@main/application/common/errors/application-error';
 
 export class RefreshTokenNotFoundError extends ApplicationError {
-  constructor() {
-    super('token not exists', 'NON_EXISTENCE_ERROR');
+  constructor(message?: string) {
+    super(message || 'token not exists', 'NON_EXISTENCE_ERROR');
     this.name = 'RefreshTokenNotFoundError';
   }
 }
