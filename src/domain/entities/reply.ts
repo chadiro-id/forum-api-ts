b038a63c54b58f5b0b49488db9f673e89dcf2887
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-11 16:03:54 +0700

    make is delete readonly

--- src/domain/entities/reply.ts
+++ src/domain/entities/reply.ts
@@ -1,8 +1,5 @@
 import { DomainEntity, EntityId } from '../common/domain-entity';
 import { DomainError } from '../common/domain-error';
-import { AccessRightsError } from '../errors/access-rights.error';
-import { AlreadyDeletedError } from '../errors/already-deleted.error';
-import { DeceptiveAccessError } from '../errors/deceptive-access.error';
 import { CommentId } from './comment';
 import { ThreadId } from './thread';
 import { UserId } from './user';
@@ -19,20 +16,16 @@ export class ReplyId extends EntityId {
 }
 
 export class Reply extends DomainEntity<ReplyId> {
-  private _isDelete: boolean;
-
   private constructor(
     id: ReplyId,
     public readonly threadId: ThreadId,
     public readonly commentId: CommentId,
     public readonly ownerId: UserId,
     public readonly content: string,
-    isDelete: boolean,
+    public readonly isDelete: boolean,
     public readonly createdAt: Date,
   ) {
     super(id);
-
-    this._isDelete = isDelete;
   }
 
   static create(
@@ -53,34 +46,5 @@ export class Reply extends DomainEntity<ReplyId> {
       isDelete,
       createdAt,
     );
-  }
-
-  get isDelete() {
-    return this._isDelete;
-  }
-
-  markAsDeleted() {
-    if (this.isDelete) {
-      throw new AlreadyDeletedError('Cannot delete reply');
-    }
-    this._isDelete = true;
-  }
-
-  verifyNonDeceptiveAccess(threadId: ThreadId, commentId: CommentId) {
-    if (!this.threadId.equals(threadId) || !this.commentId.equals(commentId)) {
-      throw new DeceptiveAccessError(
-        'Cannot access reply',
-        'RELATED_ID_NOT_MATCH',
-      );
-    }
-  }
-
-  verifyAccessRights(userId: UserId) {
-    if (!this.ownerId.equals(userId)) {
-      throw new AccessRightsError(
-        'Request id does not have access rights',
-        'USER_ID_NOT_MATCH_OWNER',
-      );
-    }
   }
 }
