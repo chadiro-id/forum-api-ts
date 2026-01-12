40bbfafb2e8d27f89ffab4cfc87e24db2a50da1d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 03:56:07 +0700

    change args and provide validations

--- src/application/threads/command/add-thread.command.ts
+++ src/application/threads/command/add-thread.command.ts
@@ -1,9 +1,19 @@
+import { ApplicationError } from '@main/application/common/errors/application-error';
 import { UserId } from '../../../domain/users/user';
 
 export class AddThreadCommand {
-  constructor(
-    public readonly title: string,
-    public readonly body: string,
-    public readonly userId: UserId,
-  ) {}
+  public readonly title: string;
+  public readonly body: string;
+  public readonly userId: UserId;
+
+  constructor(title: string, body: string, userId: string) {
+    this.userId = new UserId(userId);
+    if (!title || !body) {
+      throw new ApplicationError('missing arguments', 'ARGUMENT_ERROR');
+    }
+
+    if (typeof title !== 'string' || typeof body !== 'string') {
+      throw new ApplicationError('invalid data type', 'ARGUMENT_ERROR');
+    }
+  }
 }
