9c147761827c2ac7cb158cab011b026036038e98
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 03:07:44 +0700

    change args data type

--- src/application/comments/command/delete-comment.command.ts
+++ src/application/comments/command/delete-comment.command.ts
@@ -3,9 +3,13 @@ import { ThreadId } from '../../../domain/threads/thread';
 import { UserId } from '../../../domain/users/user';
 
 export class DeleteCommentCommand {
-  constructor(
-    public readonly id: CommentId,
-    public readonly threadId: ThreadId,
-    public readonly userId: UserId,
-  ) {}
+  public readonly id: CommentId;
+  public readonly threadId: ThreadId;
+  public readonly userId: UserId;
+
+  constructor(id: string, threadId: string, userId: string) {
+    this.id = new CommentId(id);
+    this.threadId = new ThreadId(threadId);
+    this.userId = new UserId(userId);
+  }
 }
