7eefea7e9ef4f9f56346016d29b6fca6ea881f4b
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 14:11:46 +0700

    new: add thread command handler test

---
+++ src/application/threads/command/handler/add-thread.command-handler.spec.ts
@@ -0,0 +1,34 @@
+import { UserId } from '@main/domain/entities/user';
+import { InMemoryThreadRepository } from '../../../../domain/repositories/tests/in-memory-thread-repository';
+import { ThreadRepository } from '../../../../domain/repositories/thread-repository.interface';
+import { AddThreadCommand } from '../add-thread.command';
+import { AddThreadCommandHandler } from './add-thread.command-handler';
+import { AddedThreadReport } from '../../reports/added-thread.report';
+
+describe('AddThreadCommandHandler', () => {
+  let threadRepo: ThreadRepository;
+  let commandHandler: AddThreadCommandHandler;
+
+  beforeAll(() => {
+    threadRepo = new InMemoryThreadRepository();
+    commandHandler = new AddThreadCommandHandler(
+      threadRepo,
+      () => 'thread-001',
+    );
+  });
+
+  it('should handle add thread correctly', async () => {
+    threadRepo.add = jest.fn().mockResolvedValue(undefined);
+
+    const command = new AddThreadCommand(
+      'Sebuah thread',
+      'Isi thread',
+      new UserId('user-001'),
+    );
+    const result = await commandHandler.handle(command);
+
+    expect(result).toStrictEqual(
+      new AddedThreadReport('thread-001', 'Sebuah thread', 'user-001'),
+    );
+  });
+});
