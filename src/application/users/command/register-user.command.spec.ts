0760a354abd1e3c4153e9c6eef9a3e90089b9da0
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 04:17:14 +0700

    add validations test cases

--- src/application/users/command/register-user.command.spec.ts
+++ src/application/users/command/register-user.command.spec.ts
@@ -1,3 +1,4 @@
+import { ApplicationError } from '@main/application/common/errors/application-error';
 import { RegisterUserCommand } from './register-user.command';
 
 describe('RegisterUserCommand', () => {
@@ -7,5 +8,33 @@ describe('RegisterUserCommand', () => {
     expect(command.username).toBe('johndoe');
     expect(command.password).toBe('p455w0rd');
     expect(command.fullname).toBe('John Doe');
+  });
+
+  it('should throw error when missing arguments', () => {
+    const missing = undefined as unknown as string;
+
+    expect(
+      () => new RegisterUserCommand(missing, 'p455w0rd', 'John Doe'),
+    ).toThrow(ApplicationError);
+    expect(
+      () => new RegisterUserCommand('johndoe', missing, 'John Doe'),
+    ).toThrow(ApplicationError);
+    expect(
+      () => new RegisterUserCommand('johndoe', 'p455w0rd', missing),
+    ).toThrow(ApplicationError);
+  });
+
+  it('should throw error when data type mismatch', () => {
+    const notString = 123 as unknown as string;
+
+    expect(
+      () => new RegisterUserCommand(notString, 'p455w0rd', 'John Doe'),
+    ).toThrow(ApplicationError);
+    expect(
+      () => new RegisterUserCommand('johndoe', notString, 'John Doe'),
+    ).toThrow(ApplicationError);
+    expect(
+      () => new RegisterUserCommand('johndoe', 'p455w0rd', notString),
+    ).toThrow(ApplicationError);
   });
 });
