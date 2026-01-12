c3e13aad62f9de3b50c72bd0bd3c6ea9bf27cac2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 00:20:43 +0700

    new: in memory thread details query service

---
+++ src/application/common/tests/query-service/in-memory-thread-details-query-service.ts
@@ -0,0 +1,22 @@
+import {
+  ThreadDetails,
+  CommentDetails,
+  ReplyDetails,
+} from '@main/application/threads/query/results/thread-details.result';
+import { CommentId } from '@main/domain/comments/comment';
+import { ThreadId } from '@main/domain/threads/thread';
+import { ThreadDetailsQueryService } from '../../../threads/interfaces/thread-details-query-service.interface';
+
+export class InMemoryThreadDetailsQueryService implements ThreadDetailsQueryService {
+  getById(_id: ThreadId): Promise<ThreadDetails | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  getCommentsById(_id: ThreadId): Promise<CommentDetails[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  getRepliesByCommentIds(_ids: CommentId[]): Promise<ReplyDetails[]> {
+    throw new Error('Method not implemented.');
+  }
+}
