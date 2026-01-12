bf698bac3fe64fc6377d05a57c1743bbe3519d9f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 16:25:40 +0700

    new: registered user report test

---
+++ src/application/users/reports/registered-user.report.spec.ts
@@ -0,0 +1,32 @@
+import { User, UserId } from '@main/domain/entities/user';
+import { RegisteredUserReport } from './registered-user.report';
+
+describe('RegisteredUserReport', () => {
+  it('should initialize correct property', () => {
+    const report = new RegisteredUserReport('user-001', 'johndoe', 'John Doe');
+    expect(report.id).toEqual('user-001');
+    expect(report.username).toEqual('johndoe');
+    expect(report.fullname).toEqual('John Doe');
+
+    expect(Object.keys(report)).toHaveLength(3);
+  });
+
+  describe('fromEntity', () => {
+    it('should correctly initialize data from entity', () => {
+      const entity = User.create(
+        new UserId('user-001'),
+        'johndoe',
+        'p455w0rd',
+        'John Doe',
+      );
+      const report = RegisteredUserReport.fromEntity(entity);
+      expect(report).toStrictEqual(
+        new RegisteredUserReport(
+          entity.id.value,
+          entity.username,
+          entity.fullname,
+        ),
+      );
+    });
+  });
+});
