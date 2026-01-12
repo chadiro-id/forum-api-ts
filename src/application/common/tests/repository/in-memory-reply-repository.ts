2f52ee797330d98a1acf29a8e32dbbeb5a41bfa0
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 15:05:52 +0700

    new: in memory reply repository

---
+++ src/application/common/tests/repository/in-memory-reply-repository.ts
@@ -0,0 +1,33 @@
+import { Reply, ReplyId } from '@main/domain/entities/reply';
+import { ReplyRepository } from '@main/domain/repositories/reply-repository.interface';
+
+export class InMemoryReplyRepository implements ReplyRepository {
+  constructor(private storage: Array<Reply> = []) {}
+
+  async add(reply: Reply): Promise<void> {
+    this.storage.push(reply);
+  }
+
+  async findById(id: ReplyId): Promise<Reply | null> {
+    const item = this.storage.find((r) => r.id.equals(id));
+    return item ? item : null;
+  }
+
+  async softDelete(reply: Reply): Promise<void> {
+    const item = this.storage.find((r) => r.id.equals(reply.id));
+    if (!item) return;
+
+    const newItem = Reply.create(
+      item.id,
+      item.threadId,
+      item.commentId,
+      item.ownerId,
+      item.content,
+      true,
+      item.createdAt,
+    );
+
+    const idx = this.storage.findIndex((c) => c.equals(item));
+    this.storage[idx] = newItem;
+  }
+}
