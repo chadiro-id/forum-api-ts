f9174dcebfbe95bd75d0f7610bfacabee2d5f98c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 03:32:44 +0700

    add validations test cases

--- src/application/replies/command/add-reply.command.spec.ts
+++ src/application/replies/command/add-reply.command.spec.ts
@@ -2,11 +2,12 @@ import { ThreadId } from '@main/domain/threads/thread';
 import { AddReplyCommand } from './add-reply.command';
 import { CommentId } from '@main/domain/comments/comment';
 import { UserId } from '@main/domain/users/user';
+import { ApplicationError } from '@main/application/common/errors/application-error';
 
 describe('AddReplyCommand', () => {
   it('should correctly initialize data', () => {
     const command = new AddReplyCommand(
-      'thread-d',
+      'thread-id',
       'comment-id',
       'user-id',
       'Sebuah balasan',
@@ -16,5 +17,21 @@ describe('AddReplyCommand', () => {
     expect(command.commentId).toStrictEqual(new CommentId('comment-id'));
     expect(command.userId).toStrictEqual(new UserId('user-id'));
     expect(command.content).toBe('Sebuah balasan');
+  });
+
+  it('should throw error when content not valid string', () => {
+    expect(
+      () =>
+        new AddReplyCommand(
+          'thread-id',
+          'comment-id',
+          'user-id',
+          123 as unknown as string,
+        ),
+    ).toThrow(ApplicationError);
+
+    expect(
+      () => new AddReplyCommand('thread-id', 'comment-id', 'user-id', ''),
+    ).toThrow('content cannot be empty');
   });
 });
