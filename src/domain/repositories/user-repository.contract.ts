35eb1e0666062d3cf13a7fb5cefae1d90fc824c6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 15:22:25 +0700

    update user repository contract test

--- src/domain/repositories/user-repository.contract.ts
+++ src/domain/repositories/user-repository.contract.ts
@@ -1,5 +1,4 @@
-import { Thread, ThreadId } from '../entities/thread';
-import { UserId } from '../entities/user';
+import { UserId, User } from '../entities/user';
 
 export function userRepositoryContract<T extends new (...args: any[]) => any>(
   RepoClass: T,
@@ -12,25 +11,23 @@ export function userRepositoryContract<T extends new (...args: any[]) => any>(
 
     testBehavior: () => {
       describe('UserRepository Behavior', () => {
+        const repo = new RepoClass(...params);
+
         it('should implement method correctly', () => {
           const instance = new RepoClass(...params);
 
           expect(typeof instance['add']).toBe('function');
-          expect(typeof instance['existsById']).toBe('function');
+          expect(typeof instance['existsByUsername']).toBe('function');
         });
 
         it('should enforce add method', async () => {
-          const id = new ThreadId('thread-001');
-          const ownerId = new UserId('user-002');
-          const title = 'Sebuah thread';
-          const body = 'Isi thread';
-          const thread = Thread.create(id, ownerId, title, body);
+          const id = new UserId('user-001');
+          const user = User.create(id, 'johndoe', 'p455w0rd', 'John Doe');
 
-          const repo = new RepoClass(...params);
-          await repo.add(thread);
+          await repo.add(user);
 
-          const persistedThread = await repo.existsById(id);
-          expect(persistedThread).toBe(true);
+          const exists = await repo.existsByUsername('johndoe');
+          expect(exists).toBe(true);
         });
       });
     },
