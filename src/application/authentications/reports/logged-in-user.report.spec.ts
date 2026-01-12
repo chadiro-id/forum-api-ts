785d49cfa91d576c2e2103d3cb1d8ee518359066
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 23:24:30 +0700

    update test description

--- src/application/authentications/reports/logged-in-user.report.spec.ts
+++ src/application/authentications/reports/logged-in-user.report.spec.ts
@@ -1,12 +1,10 @@
 import { LoggedInUserReport } from './logged-in-user.report';
 
 describe('LoggedInUserReport', () => {
-  it('should initialize with valid data', () => {
+  it('should correctly initialize data', () => {
     const report = new LoggedInUserReport('access_token', 'refresh_token');
 
     expect(report.accessToken).toBe('access_token');
     expect(report.refreshToken).toBe('refresh_token');
-
-    expect(Object.keys(report)).toHaveLength(2);
   });
 });
