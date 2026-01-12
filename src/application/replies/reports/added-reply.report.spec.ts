21fb58c23ed6992499b44a9c5d2a97dde2b44cdf
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 22:19:34 +0700

    new: added reply report unit test

---
+++ src/application/replies/reports/added-reply.report.spec.ts
@@ -0,0 +1,36 @@
+import { Reply, ReplyId } from '@main/domain/replies/reply';
+import { AddedReplyReport } from './added-reply.report';
+import { ThreadId } from '@main/domain/threads/thread';
+import { CommentId } from '@main/domain/comments/comment';
+import { UserId } from '@main/domain/users/user';
+
+describe('AddedReplyReport', () => {
+  it('should initialize with valid data', () => {
+    const report = new AddedReplyReport(
+      'reply-id',
+      'Sebuah balasan',
+      'user-id',
+    );
+
+    expect(report.id).toBe('reply-id');
+    expect(report.content).toBe('Sebuah balasan');
+    expect(report.owner).toBe('user-id');
+  });
+
+  describe('fromEntity', () => {
+    it('should initialize with data from from reply entity', () => {
+      const entity = Reply.create(
+        new ReplyId('reply-id'),
+        new ThreadId('thread-id'),
+        new CommentId('comment-id'),
+        new UserId('user-id'),
+        'Sebuah balasan',
+      );
+
+      const report = AddedReplyReport.fromEntity(entity);
+      expect(report).toStrictEqual(
+        new AddedReplyReport('reply-id', 'Sebuah balasan', 'user-id'),
+      );
+    });
+  });
+});
