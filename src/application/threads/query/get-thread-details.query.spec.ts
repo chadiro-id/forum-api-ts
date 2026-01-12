bf2862f7146855b33382cb0478032a38c4606cea
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 02:03:27 +0700

    new: get thread details query unit test

---
+++ src/application/threads/query/get-thread-details.query.spec.ts
@@ -0,0 +1,9 @@
+import { ThreadId } from '@main/domain/threads/thread';
+import { GetThreadDetailsQuery } from './get-thread-details.query';
+
+describe('GetThreadDetailsQuery', () => {
+  it('should correctly initialize data', () => {
+    const query = new GetThreadDetailsQuery('thread-id');
+    expect(query.id).toStrictEqual(new ThreadId('thread-id'));
+  });
+});
