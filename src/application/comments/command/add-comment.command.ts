00acc330f047b547542e1a9abc57f86de67d965e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 02:52:44 +0700

    provide argument validations

--- src/application/comments/command/add-comment.command.ts
+++ src/application/comments/command/add-comment.command.ts
@@ -1,10 +1,22 @@
+import { ApplicationError } from '@main/application/common/errors/application-error';
 import { ThreadId } from '../../../domain/threads/thread';
 import { UserId } from '../../../domain/users/user';
 
 export class AddCommentCommand {
-  constructor(
-    public readonly threadId: ThreadId,
-    public readonly userId: UserId,
-    public readonly content: string,
-  ) {}
+  public readonly threadId: ThreadId;
+  public readonly userId: UserId;
+  public readonly content: string;
+
+  constructor(threadId: string, userId: string, content: string) {
+    this.threadId = new ThreadId(threadId);
+    this.userId = new UserId(userId);
+    this.content = this.validateContent(content);
+  }
+
+  private validateContent(value: unknown): string {
+    if (!value || typeof value !== 'string') {
+      throw new ApplicationError('content cannot be empty', 'ARGUMENT_ERROR');
+    }
+    return value;
+  }
 }
