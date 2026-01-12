d152c2d985d1c7098ff9dbfdd92f092aea306508
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-12 11:52:10 +0700

    use application error filter

--- src/infrastructure/http/create-http-app.ts
+++ src/infrastructure/http/create-http-app.ts
@@ -1,8 +1,8 @@
 import { JoiValidationPipe } from '../../presentation/shared/pipes/joi-validation.pipe';
 import { MyExpressApp } from '../../libs/my-app/core/my-express-app';
 import { ClientErrorFilter } from '../filters/client-error.filter';
-import { DomainErrorFilter } from '../filters/domain-error.filter';
 import { WrapResponseInterceptor } from '../interceptors/wrap-response.interceptor';
+import { ApplicationErrorFilter } from '../filters/application-error.filter';
 
 export type CreateHttpAppOptions = {
   enableError: boolean;
@@ -10,7 +10,7 @@ export type CreateHttpAppOptions = {
 
 export function createHttpApp(module: any, options?: CreateHttpAppOptions) {
   const app = new MyExpressApp(module);
-  app.useGlobalFilters(DomainErrorFilter, ClientErrorFilter);
+  app.useGlobalFilters(ApplicationErrorFilter, ClientErrorFilter);
   app.useGlobalPipes(JoiValidationPipe);
   app.useGlobalInterceptors(WrapResponseInterceptor);
 
