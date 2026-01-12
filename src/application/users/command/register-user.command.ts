bff7ee905eeb5b1a915a98f9bb15b9a77f1ddc35
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 04:11:59 +0700

    update args validations

--- src/application/users/command/register-user.command.ts
+++ src/application/users/command/register-user.command.ts
@@ -6,29 +6,16 @@ export class RegisterUserCommand {
     public readonly password: string,
     public readonly fullname: string,
   ) {
-    this.validate();
-  }
-
-  private validate() {
-    if (!this.username || typeof this.username !== 'string') {
-      throw new ApplicationError(
-        'username must be non-empty string',
-        'ARGUMENT_ERROR',
-      );
+    if (!this.username || !this.password || !this.fullname) {
+      throw new ApplicationError('missing arguments', 'ARGUMENT_ERROR');
     }
 
-    if (!this.password || typeof this.password !== 'string') {
-      throw new ApplicationError(
-        'password must be non-empty string',
-        'ARGUMENT_ERROR',
-      );
-    }
-
-    if (!this.fullname || typeof this.fullname !== 'string') {
-      throw new ApplicationError(
-        'password must be non-empty string',
-        'ARGUMENT_ERROR',
-      );
+    if (
+      typeof username !== 'string' ||
+      typeof password !== 'string' ||
+      typeof fullname !== 'string'
+    ) {
+      throw new ApplicationError('invalid data type', 'ARGUMENT_ERROR');
     }
   }
 }
