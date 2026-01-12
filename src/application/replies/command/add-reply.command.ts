7810e9f7f36589f497c32a0abfabf6f516b3aa2f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 03:22:40 +0700

    change args data type

--- src/application/replies/command/add-reply.command.ts
+++ src/application/replies/command/add-reply.command.ts
@@ -1,12 +1,30 @@
+import { ApplicationError } from '@main/application/common/errors/application-error';
 import { CommentId } from '../../../domain/comments/comment';
 import { ThreadId } from '../../../domain/threads/thread';
 import { UserId } from '../../../domain/users/user';
 
 export class AddReplyCommand {
+  public readonly threadId: ThreadId;
+  public readonly commentId: CommentId;
+  public readonly userId: UserId;
+  public readonly content: string;
+
   constructor(
-    public readonly threadId: ThreadId,
-    public readonly commentId: CommentId,
-    public readonly userId: UserId,
-    public readonly content: string,
-  ) {}
+    threadId: string,
+    commentId: string,
+    userId: string,
+    content: string,
+  ) {
+    this.threadId = new ThreadId(threadId);
+    this.commentId = new CommentId(commentId);
+    this.userId = new UserId(userId);
+    this.content = this.validateContent(content);
+  }
+
+  private validateContent(value: unknown): string {
+    if (!value || typeof value !== 'string') {
+      throw new ApplicationError('content cannot be empty', 'ARGUMENT_ERROR');
+    }
+    return value;
+  }
 }
