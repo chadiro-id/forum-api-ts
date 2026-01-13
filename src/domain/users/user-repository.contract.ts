eb7fe765bb0551b4baaa959f9f2540cf8e16d53f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 17:37:36 +0700

    update user repository contract

--- src/domain/users/user-repository.contract.ts
+++ src/domain/users/user-repository.contract.ts
@@ -1,31 +1,28 @@
 import { UserId, User } from './entities/user';
+import { UserRepository } from './user-repository.interface';
 
 export function userRepositoryContract<T extends new (...args: any[]) => any>(
   RepoClass: T,
   ...params: ConstructorParameters<T>
 ) {
   return {
-    createInstance: () => {
-      return new RepoClass(...params);
-    },
-
     testBehavior: () => {
       describe('UserRepository Behavior', () => {
-        const repo = new RepoClass(...params);
-
-        it('should implement method correctly', () => {
-          const instance = new RepoClass(...params);
-
-          expect(typeof instance['add']).toBe('function');
-          expect(typeof instance['existsByUsername']).toBe('function');
-        });
-
-        it('should enforce add method', async () => {
+        it('should implement method correctly', async () => {
           const id = new UserId('user-001');
           const user = User.create(id, 'johndoe', 'p455w0rd', 'John Doe');
 
+          const repo = new RepoClass(...params) as UserRepository;
+
+          const nonExistsUser = await repo.findByUsername('johndoe');
+          expect(nonExistsUser).toBe(null);
+          const nonExists = await repo.existsByUsername('johndoe');
+          expect(nonExists).toBe(false);
+
           await repo.add(user);
 
+          const foundUser = await repo.findByUsername('johndoe');
+          expect(foundUser).toStrictEqual(user);
           const exists = await repo.existsByUsername('johndoe');
           expect(exists).toBe(true);
         });
