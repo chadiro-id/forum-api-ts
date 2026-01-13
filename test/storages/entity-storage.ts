0f844d4cc6b50134d9deb27d1c7ea2624909aa05
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-14 00:37:37 +0700

    in memory db

---
+++ test/storages/entity-storage.ts
@@ -0,0 +1,13 @@
+import { DomainEntity, EntityId } from '@main/domain/common/domain-entity';
+
+export type StorageKey =
+  | 'users'
+  | 'authentications'
+  | 'threads'
+  | 'comments'
+  | 'replies';
+
+export type EntityStorage = Map<
+  StorageKey,
+  DomainEntity<EntityId<string | number>>[]
+>;
