263c156bbd8a158ebcd7c0577cdd96643ccf1f97
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-13 18:13:56 +0700

    update bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,15 @@
 import 'reflect-metadata';
-// import expressListRoutes from 'express-list-routes';
 import { AppModule } from './app.module';
-import { MyExpressApp } from './libs/my-app/core/my-express-app';
-import { WrapResponseInterceptor } from './infrastructure/interceptors/wrap-response.interceptor';
-import { ClientErrorFilter } from './infrastructure/filters/client-error.filter';
-import { LoggingInterceptor } from './infrastructure/interceptors/logging.interceptor';
-import { JoiValidationPipe } from './presentation/shared/pipes/joi-validation.pipe';
-import { ApplicationErrorFilter } from './infrastructure/filters/application-error.filter';
+import { createHttpApp } from './infrastructure/http/create-http-app';
 
 async function bootstrap() {
-  const app = new MyExpressApp(AppModule);
-  app
-    .useGlobalPipes(JoiValidationPipe)
-    .useGlobalInterceptors(WrapResponseInterceptor, LoggingInterceptor)
-    .useGlobalFilters(ApplicationErrorFilter, ClientErrorFilter);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const host = process.env.HOST ?? 'localhost';
 
-  app.listen(
-    process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    process.env.HOST ?? 'localhost',
-    () => {
-      console.log('Server running on port');
-    },
-  );
-  // console.log('express routes', expressListRoutes(app.getApp()));
+  const app = createHttpApp(AppModule);
+  app.listen(port, host, () => {
+    console.log(`Server running on port: ${port}`);
+  });
 }
 
 bootstrap().catch((error) => console.error(error));
