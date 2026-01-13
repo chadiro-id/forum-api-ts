10cf1f8e31149c5fd8616431e389dd2e54af74ff
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-14 02:05:47 +0700

    create separate file for mock thread query service

--- src/application/threads/interfaces/thread-details-query-service.spec.ts
+++ src/application/threads/interfaces/thread-details-query-service.spec.ts
@@ -1,35 +1,6 @@
 import { CommentId } from '@main/domain/comments/entities/comment';
 import { ThreadId } from '@main/domain/threads/entities/thread';
-import {
-  ThreadDetails,
-  CommentDetails,
-  ReplyDetails,
-} from '../query/results/thread-details.result';
-import { ThreadDetailsQueryService } from './thread-details-query-service.interface';
-
-export class MockThreadDetailsQueryService implements ThreadDetailsQueryService {
-  private threadDetailsList: Array<ThreadDetails> = [];
-  private commentDetailsList: Array<CommentDetails> = [];
-  private replyDetailsList: Array<ReplyDetails> = [];
-
-  async getById(id: ThreadId): Promise<ThreadDetails | null> {
-    const item = this.threadDetailsList.find((t) => t.id.equals(id));
-    return item ?? null;
-  }
-
-  async getCommentsById(id: ThreadId): Promise<CommentDetails[]> {
-    const comments = this.commentDetailsList.filter((c) => c.id.equals(id));
-    return comments;
-  }
-
-  async getRepliesByCommentIds(ids: CommentId[]): Promise<ReplyDetails[]> {
-    const valueIds = ids.map((id) => id.value);
-    const replies = this.replyDetailsList.filter((r) =>
-      valueIds.includes(r.commentId.value),
-    );
-    return replies;
-  }
-}
+import { MockThreadDetailsQueryService } from './thread-details-query-service.mock';
 
 describe('ThreadDetailsQueryService', () => {
   it('should enforce getById method', async () => {
