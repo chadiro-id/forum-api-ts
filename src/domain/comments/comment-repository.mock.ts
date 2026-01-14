be0a6fdc95cfb87073c20ab2a2125dc184576d6d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2026-01-14 19:50:30 +0700

    update exists by code

--- src/domain/comments/comment-repository.mock.ts
+++ src/domain/comments/comment-repository.mock.ts
@@ -1,6 +1,5 @@
 import { CommentRepository } from './comment-repository.interface';
 import { Comment, CommentId } from './entities/comment';
-import { EntityId } from '../common/domain-entity';
 
 export class MockCommentRepository implements CommentRepository {
   private commentList: Array<Comment> = [];
@@ -15,19 +14,16 @@ export class MockCommentRepository implements CommentRepository {
   }
 
   async existsBy(criteria: Partial<Comment>): Promise<boolean> {
-    const comments = this.commentList.filter((c) => {
-      for (const [k, v] of Object.entries(criteria)) {
-        if (v instanceof EntityId && c[k].equals(v)) return true;
-        if (c[k] === v) return true;
-      }
-      return false;
-    });
+    const comments = this.commentList.filter((c) =>
+      Object.keys(criteria).every((k) =>
+        c[k].equals ? c[k].equals(criteria[k]) : c[k] === criteria[k],
+      ),
+    );
     return comments.length > 0;
   }
 
   async softDelete(comment: Comment): Promise<void> {
     const idx = this.commentList.findIndex((c) => c.equals(comment));
-    if (idx < 0) return;
     this.commentList[idx] = Comment.create(
       comment.id,
       comment.threadId,
